b2ea48a3a7a038ce923a4d0387797d61bbf28a21
Charles Hatch <charlesghatch@gmail.com>
2018-07-12 10:59:52 -0600

    add test 1,2, passed

--- functions.test.js
+++ functions.test.js
@@ -11,3 +11,14 @@ test('greeting("James") should return correct string', () => {
   let str = fns.greeting("James");
   expect(str).toEqual("Hello, James.");
 });
+
+
+test('expect add(1, 2) should return 3', () => {
+    let num = fns.add(1,2);
+    expect(num).toBe(3);
+})
+
+test('expect add(5, 9) should return 14', () => {
+    let num = fns.add(5,9);
+    expect(num).toBe(14);
+})
